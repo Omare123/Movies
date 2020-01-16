6ca8f45d2775e2561bae3cdb5b3482968b8bc6e7
Omar Rojas <orojas@in.hq.binit.com.ar>
2020-01-16 10:54:45 -0300

    Add project files.

---
+++ WebApplication1/Models/Dominio/Pelicula.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Models.Dominio
+{
+    public class Pelicula : Entidad
+    {
+        public Pelicula()
+        {
+        }
+
+        public Pelicula(DateTime fechaestreno, int duracion, string genero) {
+
+            FechaEstreno = fechaestreno;
+            Duracion = duracion;
+            Genero = genero;
+        }
+
+        public DateTime FechaEstreno { get; set; }
+        public int Duracion { get; set; }
+        public string Genero { get; set; }
+
+    }
+}
---
+++ WebApplication1/Models/Interfaces/IEntidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InduccionV7.Website.Models.Interfaces
+{
+    public interface IEntidad
+    {
+        Guid Id { get; set; }
+        string Nombre { get; set; }
+        DateTime FechaCreacion { get; set; }
+        DateTime FechaEdicion { get; set; }
+        bool Eliminado { get; set; }
+
+    }
+}
